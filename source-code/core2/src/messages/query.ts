4e935872b3d68eb271d706354866d4d85f63c122
Ivan Hofer <ivan.hofer@outlook.com>
2023-07-25 16:14:09 +0200

    add basic implementation

--- source-code/core2/src/messages/query.ts
+++ source-code/core2/src/messages/query.ts
@@ -1,8 +1,10 @@
+import { createSignal, createEffect } from 'solid-js'
 import type { Message } from "./schema.js"
 
 export type MessageQueryApi = {
+	subscribe: (fn: (message: Message[]) => void) => void
 	create: (args: { data: Message }) => void
-	get: (args: { where: { id: Message["id"] } }) => Message | undefined
+	get: (args: { where: { id: Message["id"] } }) => { subscribe: (fn: (message: Message | undefined) => void) => void }
 	update: (args: { where: { id: Message["id"] }; data: Partial<Message> }) => void
 	upsert: (args: { where: { id: Message["id"] }; data: Message }) => void
 	delete: (args: { where: { id: Message["id"] } }) => void
@@ -13,29 +15,55 @@ export type MessageQueryApi = {
  *
  * Creates an index internally for faster get operations.
  */
-export function createQuery(messages: Array<Message>): MessageQueryApi {
-	const index = new Map(messages.map((message) => [message.id, message]))
+export function createQuery(messagesInit: Array<Message>): MessageQueryApi {
+	const [messages, setMessages] = createSignal<Message[]>(messagesInit)
+
+	const callbacks: ((message: Message[]) => void)[] = []
+
+	createEffect(() => {
+		for (const callback of callbacks) callback(messages())
+	})
+
 	return {
-		create: ({ data }) => {
-			index.set(data.id, data)
+		subscribe: (callback: (message: Message[]) => void) => {
+			callbacks.push(callback)
 		},
+		// query
 		get: ({ where }) => {
-			return index.get(where.id)
+			const callbacks: ((message: Message | undefined) => void)[] = []
+			const [message, setMessage] = createSignal<Message | undefined>()
+
+			createEffect(() => {
+				setMessage(messages().find(({ id }) => id === where.id))
+			})
+
+			createEffect(() => {
+				for (const callback of callbacks) callback(message())
+			})
+
+			return {
+				subscribe: (callback: (message: Message | undefined) => void) => {
+					callbacks.push(callback)
+				},
+			}
+		},
+		// mutation
+		create: ({ data }) => {
+			setMessages([...messages(), data])
 		},
 		update: ({ where, data }) => {
-			const message = index.get(where.id)
-			if (message === undefined) return
-			index.set(where.id, { ...message, ...data })
+			setMessages(messages().map(m => (m.id === where.id ? { ...m, ...data } : m)))
 		},
 		upsert: ({ where, data }) => {
-			const message = index.get(where.id)
+			const message = messages().find(({ id }) => id === where.id)
 			if (message === undefined) {
-				return index.set(where.id, data)
+				setMessages([...messages(), data])
+			} else {
+				setMessages(messages().map(m => (m.id === where.id ? { ...m, ...data } : m)))
 			}
-			index.set(where.id, { ...message, ...data })
 		},
 		delete: ({ where }) => {
-			index.delete(where.id)
+			setMessages(messages().filter(({ id }) => id !== where.id))
 		},
 	}
 }
